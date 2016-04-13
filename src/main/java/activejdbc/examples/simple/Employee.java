8358d419cae5bb7e1b7c87b53f5c096cce0e7054
Jason Maher <jason@jasonmaher.me>
2016-04-13 14:50:08 +0900

    add jackson annotations to the model class

--- src/main/java/activejdbc/examples/simple/Employee.java
+++ src/main/java/activejdbc/examples/simple/Employee.java
@@ -1,9 +1,25 @@
 package activejdbc.examples.simple;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.javalite.activejdbc.Model;
 
+import java.io.IOException;
+
+@JsonSerialize(using = Employee.Serializer.class)
 public class Employee extends Model {
     static{
         validatePresenceOf("first_name", "last_name");
     }
+
+    public static class Serializer extends JsonSerializer<Employee> {
+        @Override
+        public void serialize(Employee value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
+            gen.writeRawValue(value.toJson(false));
+        }
+    }
+
 }
